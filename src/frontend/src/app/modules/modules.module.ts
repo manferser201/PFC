8f89a0ce3b077892339999e8980f22a8c0a64d24
manferser201 <manferser201@gmail.com>
2023-03-23 12:15:13 +0100

    Creación y enrutado de Home y Registration

--- src/frontend/src/app/modules/modules.module.ts
+++ src/frontend/src/app/modules/modules.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    RegistrationComponent
+  ],
   imports: [
     CommonModule
   ]
 })
+
 export class ModulesModule { }
