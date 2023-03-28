89038b02cd52d7bacbec377e2b4a811331622d6e
manferser201 <manferser201@gmail.com>
2023-03-28 13:32:34 +0200

    Instalación express-validator y  validaciones user

--- src/backend/routes/users.js
+++ src/backend/routes/users.js
@@ -1,17 +1,56 @@
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
+const { body, validationResult } = require('express-validator');
 let User = require('../models/User');
 
 // POST de un nuevo usuario
 router.post('/', 
   
+  body('username')
+    .exists()
+    .isString(),
+  body('password')
+    .exists()
+    .isAlphanumeric()
+    .isLength( {min:8} ),
+  body('name')
+    .exists()
+    .isString(),
+  body('surname')
+    .exists()
+    .isString(),
+  body('identification')
+    .exists()
+    .isAlphanumeric(),
+  body('email')
+    .exists()
+    .isEmail(),
+  body('phone_number')
+    .optional()
+    .isLength(9),
+  body('adress')
+    .exists()
+    .isString(),
+  body('birthday')
+    .exists()
+    .isDate(),
+  body('pay')
+    .exists()
+    .isString(),
+  body('photo')
+    .optional()
+    .isString(),
+  body('rol')
+    .optional()
+    .isString(),
+
   (req, res) => {
-    // const errors = validationResult(req);
+    const errors = validationResult(req);
     
-    // if (!errors.isEmpty()){
-    //   return res.status(400).json({errors: errors.array()})
-    // }
+    if (!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array()})
+    }
 
     User.create({
 
