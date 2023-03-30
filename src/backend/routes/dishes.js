634b1b4939769ec91f95c6f63a0a725fb592b5e8
manferser201 <manferser201@gmail.com>
2023-03-30 12:34:51 +0200

    Creación método GET de un listado de los platos

--- src/backend/routes/dishes.js
+++ src/backend/routes/dishes.js
@@ -4,6 +4,7 @@ let mongoose = require('mongoose');
 const { body, validationResult } = require('express-validator');
 let Dish = require('../models/Dish');
 
+/* POST de un nuevo plato */
 router.post('/', 
 
 
@@ -22,8 +23,25 @@ router.post('/',
             num_ratings: req.body.num_ratings,
             agent: req.body.agent
 
-        }).then(user => res.json(user));
+        }).then(dish => res.json(dish));
     }
 );
+
+/* GET de todos los platos */
+router.get('/dishesList', function(req, res) {
+    Dish.find().exec(function(err, dishInfo){
+        if (err) res.status(500).send(err);
+        else res.status(200).json(dishInfo);
+    })
+});
+
+/* GET de los datos del vendedor de un plato concreto */
+
+
+/* PUT de un plato concreto */
+
+
+/* DELETE de un plato concreto */
+
 
 module.exports = router;
