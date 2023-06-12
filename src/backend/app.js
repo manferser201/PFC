b739e5f2b5c4123f9217099a38e7f0a157d6531d
Manuel Fernández Serrano <alum.mfernandezs.2021@iesalixar.org>
2023-06-12 19:05:13 +0200

    cambiosn ruta archivos estáticos

--- src/backend/app.js
+++ src/backend/app.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'backend/upload')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Modelos
 let userModel = require('./models/User');
--- src/backend/multer.js
+++ src/backend/multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '/upload'));
+        cb(null, path.join(__dirname, '/public/upload'));
     },
     filename: (req, file, cb) => {
         cb(null, `${file.fieldname} - ${Date.now()}.${file.mimetype.split('/')[1]}`)
