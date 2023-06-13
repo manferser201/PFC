cb17dc464a6f6dae88611251460b0e021f4d66bf
Manuel Fernández Serrano <alum.mfernandezs.2021@iesalixar.org>
2023-06-13 14:34:33 +0200

    cambios para intentar arreglar el upload

--- src/backend/multer.js
+++ src/backend/multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'upload');
+        cb(null, 'public');
     },
     filename: (req, file, cb) => {
         console.log("file multer", file);
